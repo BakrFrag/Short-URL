d4d95b31902b804867a36ab7220345364280d113
bk <bakr.frag@gmail.com>
2022-03-04 09:06:03 +0200

    add shorten model include both original and short urls

--- ShorentUrl/url/models.py
+++ ShorentUrl/url/models.py
@@ -1,3 +1,40 @@
+from uuid import uuid4
 from django.db import models
 
 # Create your models here.
+class Shorten(models.Model):
+    """
+    model reprsents shorten url attributes 
+    """
+    original_url = models.URLField(unique=True);
+    shorten_url = models.URLField(unique=True);
+
+    def __str__(self):
+        """
+        
+        """
+        return self.shorten_url;
+
+    def generate_short_url(self):
+        """
+        generate shorten url from random alphanumeric 
+        """
+        short_url=''
+        while True:
+            random_alpha_char= str(uuid4())[0:8];
+            if Shorten.objects.filter(shorten_url=random_alpha_char).exists():
+                continue;
+            short_url = random_alpha_char;
+            break;
+        return short_url
+    def save(self,*args,**kwargs):
+        """
+        overide save method to include shorten_url as random string of alpha numeric 
+        """
+        
+        try:
+                 obj=Shorten.objects.get(original_url=self.original_url)
+                 self.shorten_url=obj.shorten_url
+        except:
+                 self.shorten_url=self.generate_short_url();
+                 return super().save(*args,**kwargs);
